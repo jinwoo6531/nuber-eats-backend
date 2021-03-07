4e9d2dfad6a9cae6dc951a7cafa8565d17fb5307
jinwoo6531 <jintakk6531@gmail.com>
2021-03-07 13:41:06 +0900

    3/7 user 모듈 생성완료

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+
+@Resolver(of => User)
+export class UsersResolver {
+    constructor(
+        private readonly usersService: UsersService
+    ) { }
+
+    @Query(returs => Boolean)
+    hi() {
+        return true;
+    }
+}
