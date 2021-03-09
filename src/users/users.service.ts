71a3007246bd954fdef2e74c03142588056a69ab
jinwoo6531 <jintakk6531@gmail.com>
2021-03-09 11:40:29 +0900

    3/9 jwt 적용

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as jwt from 'jsonwebtoken';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class UsersService {
+export class UserService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly config: ConfigService,
   ) {}
 
   async createAccount({
@@ -27,11 +30,11 @@ export class UsersService {
       return { ok: false, error: "Couldn't create account" };
     }
   }
-
   async login({
     email,
     password,
   }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // make a JWT and give it to the user
     try {
       const user = await this.users.findOne({ email });
       if (!user) {
@@ -47,15 +50,16 @@ export class UsersService {
           error: 'Wrong password',
         };
       }
+      const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
+      return {
+        ok: true,
+        token: 'lalalalaalala',
+      };
     } catch (error) {
       return {
         ok: false,
         error,
       };
     }
-    return {
-      ok: true,
-      token: 'lalalalalalal',
-    };
   }
 }
